a174a90935c266d096274b1574705df94a5ecae3
Elizabeth <chabelina2003@gmail.com>
2023-10-20 11:17:22 -0500

    Implemented Firebase services
    This commit adds Firebase authentication and Firestore configuration files to the project.
    
    - Introduces authentication functionality in 'src/firebase/auth.js'.
    - Adds Firebase configuration in 'src/firebase/firebase-config.js'.
    - Includes Firestore setup in 'src/firebase/firestore.js'.
    
    This integration allows the project to leverage Firebase services for user authentication and data storage.

---
+++ src/firebase/firebase-config.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// import { getAnalytics } from 'firebase/analytics';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyD_UxhD_bFrHamWl41O1uLctTEi6LMxANE',
+  authDomain: 'bmusic-social-network.firebaseapp.com',
+  projectId: 'bmusic-social-network',
+  storageBucket: 'bmusic-social-network.appspot.com',
+  messagingSenderId: '980840353303',
+  appId: '1:980840353303:web:e4ad8d6cbafb844fe5d652',
+  measurementId: 'G-L597S8W8SD',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
