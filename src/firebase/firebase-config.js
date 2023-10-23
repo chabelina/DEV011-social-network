b0d359d274cb13cfc5ada84d0caa8da5498bf47b
Elizabeth <chabelina2003@gmail.com>
2023-10-22 23:31:52 -0500

    Intializing the Auth class for the App

--- src/firebase/firebase-config.js
+++ src/firebase/firebase-config.js
@@ -1,11 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
-// import { getAnalytics } from 'firebase/analytics';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: 'AIzaSyD_UxhD_bFrHamWl41O1uLctTEi6LMxANE',
   authDomain: 'bmusic-social-network.firebaseapp.com',
@@ -17,5 +12,5 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+export const app = initializeApp(firebaseConfig);
 // const analytics = getAnalytics(app);
