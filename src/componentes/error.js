663b9f5a0839d985878601bb0a2e60d3fddff2b7
Elizabeth <chabelina2003@gmail.com>
2023-10-23 01:21:05 -0500

    Fixed formatting/lint errors

--- src/componentes/error.js
+++ src/componentes/error.js
@@ -1,8 +1,6 @@
 // aqui exportaras las funciones que necesites
 
 export const error = () => {
-  // aqui tu codigo
-  //console.log('404 no se pudo encontrar la página');
   const title = document.createElement('h2');
   title.textContent = 'Error 404 page no found, please go home';
   return title;
