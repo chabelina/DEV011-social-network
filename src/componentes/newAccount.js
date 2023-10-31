ae0f4a119fda6e0ea3d0527603f0f285743f0ea5
Elizabeth <chabelina2003@gmail.com>
2023-10-31 10:51:30 -0500

    Agregando animación y sombreado para botones on hover

--- src/componentes/newAccount.js
+++ src/componentes/newAccount.js
@@ -1,31 +1,31 @@
 // aqui exportaras las funciones que necesites
 import logo from '../img/logo.png';
-import  mostrar  from '../img/mostrar.svg';
-import  noMostrar  from '../img/no-mostrar.svg';
+import mostrar from '../img/mostrar.svg';
+import noMostrar from '../img/no-mostrar.svg';
 import { createUser } from '../firebase/auth.js';
 import { inputsFormats } from '../validations/validLogin';
-//import { async } from 'regenerator-runtime';
+// import { async } from 'regenerator-runtime';
 // import { insertInfoNewUserDB } from '../firebase/firestore';
 
 // Función que renderea la vista de inicio de sesión
 export const newAccount = (navigateTo) => {
   let currentUser; // inicializamos un usuario
-  
+
   // Contenedor general
   const containerAll = document.createElement('div');
   containerAll.className = 'containerAll';
 
   // from que contiene los inputs y submits necesarios para crear una cuenta nueva
-  const formInputCreateAccount = document.createElement('form')
+  const formInputCreateAccount = document.createElement('form');
   formInputCreateAccount.method = 'get';
   formInputCreateAccount.className = 'formLogin';
 
   // Logo de B-Music
   const imgLogo = document.createElement('img');
-  imgLogo.alt = "B-Music"
-  imgLogo.setAttribute('src',logo)
-  imgLogo.className = 'logo'
-  formInputCreateAccount.appendChild(imgLogo)
+  imgLogo.alt = 'B-Music';
+  imgLogo.setAttribute('src', logo);
+  imgLogo.className = 'logo';
+  formInputCreateAccount.appendChild(imgLogo);
 
   // Caja de ingreso de Alias
   const inputNickname = document.createElement('input');
@@ -38,59 +38,58 @@ export const newAccount = (navigateTo) => {
   inputEmail.setAttribute('placeholder', 'Correo');
 
   inputEmail.id = 'inputEmail';
-  inputEmail.autocomplete = 'inputEmail';  
+  inputEmail.autocomplete = 'inputEmail';
   formInputCreateAccount.appendChild(inputEmail);
 
   const passwordContainer = document.createElement('div');
   passwordContainer.classList.add('password-container'); // Agrega una clase para contener los elementos
   formInputCreateAccount.appendChild(passwordContainer);
 
-  // Caja de ingreso de contraseña  
-  const passwordInput = document.createElement('input');
-  passwordInput.setAttribute('placeholder', 'Contraseña');
-  passwordInput.type = 'password';
-  passwordInput.id = 'inputPassword';
-  inputPassword.autocomplete = 'current-password';  
-  passwordContainer.appendChild(passwordInput);
-   // Mensaje de error al ingresar con email
+  // Caja de ingreso de contraseña
+  const inputPassword = document.createElement('input');
+  inputPassword.setAttribute('placeholder', 'Contraseña');
+  inputPassword.type = 'password';
+  inputPassword.id = 'inputPassword';
+  inputPassword.autocomplete = 'current-password';
+  passwordContainer.appendChild(inputPassword);
+  // Mensaje de error al ingresar con email
   const errorInvalidPassword = document.createElement('p');
   errorInvalidPassword.id = 'errorMessage';
-  formInputCreateAccount.append(inputPassword, errorInvalidPassword); 
+  formInputCreateAccount.append(inputPassword, errorInvalidPassword);
 
   // Check para mostrar contraseña
   const showPasswordContainer = document.createElement('div');
   showPasswordContainer.classList.add('show-password-container'); // Agrega una clase para contener los elementos
   passwordContainer.appendChild(showPasswordContainer);
-  
+
   const showPassword = document.createElement('button'); // Cambiado de input a button para que no saliera un espacio en blanco
   showPassword.type = 'button'; // Cambiado de 'checkbox' a 'button'
   showPassword.id = 'showPassword';
 
   showPassword.classList.add('show-password-button'); // Agrega una clase para estilizar el botón
-  passwordInput.appendChild(showPassword)
+  inputPassword.appendChild(showPassword);
   showPasswordContainer.appendChild(showPassword);
-  
+
   const hidePasswordIcon = document.createElement('img');
-  hidePasswordIcon.src = noMostrar; // Reemplaza 'ruta_de_icono_no_mostrar' con la ruta real del icono de "no mostrar"
-  hidePasswordIcon.alt = 'Ocultar contrase;a'
+  hidePasswordIcon.src = noMostrar; // Reemplaza 'ruta_de_icono_no_mostrar' con la ruta real del icono de 'no mostrar'
+  hidePasswordIcon.alt = 'Ocultar contraseña';
   hidePasswordIcon.classList.add('hide-password-icon'); // Agrega una clase para estilizar el icono
-  hidePasswordIcon.style.display = 'none'
+  hidePasswordIcon.style.display = 'none';
   showPassword.appendChild(hidePasswordIcon);
-  
+
   const showPasswordIcon = document.createElement('img');
-  showPasswordIcon.src = mostrar; // Reemplaza 'ruta_de_icono_mostrar' con la ruta real del icono de "mostrar"
-  showPasswordIcon.alt = 'Mostrar contrase;a'
+  showPasswordIcon.src = mostrar; // Reemplaza 'ruta_de_icono_mostrar' con la ruta real del icono de 'mostrar'
+  showPasswordIcon.alt = 'Mostrar contrase;a';
   showPasswordIcon.classList.add('show-password-icon'); // Agrega una clase para estilizar el icono
   showPassword.appendChild(showPasswordIcon);
-  
+
   showPassword.addEventListener('click', () => {
-    const isPasswordVisible = passwordInput.type === 'text';
-    
-    passwordInput.type = isPasswordVisible ? 'password' : 'text';
+    const isPasswordVisible = inputPassword.type === 'text';
+
+    inputPassword.type = isPasswordVisible ? 'password' : 'text';
     hidePasswordIcon.style.display = isPasswordVisible ? 'none' : 'flex';
-    showPasswordIcon.style.display = isPasswordVisible ? 'flex' : 'none'
+    showPasswordIcon.style.display = isPasswordVisible ? 'flex' : 'none';
   });
-  
 
   // Botón para crear cuenta
   const buttonCreateNewAccount = document.createElement('button');
@@ -100,17 +99,18 @@ export const newAccount = (navigateTo) => {
   buttonCreateNewAccount.classList.add('ingresar');
   formInputCreateAccount.appendChild(buttonCreateNewAccount);
 
-  buttonCreateNewAccount.addEventListener('click', async(e) => {
-    try{  
+  buttonCreateNewAccount.addEventListener('click', async (e) => {
+    try {
       inputsFormats(inputEmail, inputPassword); // valida que las entradas sean correctas...
-      currentUser = await createUser(inputEmail.value, inputPassword.value)  //Crea el usuario e ingresa
+      currentUser = await createUser(inputEmail.value, inputPassword.value); //Crea el usuario e ingresa
       navigateTo('/publications'); // Se mueve a la vista de publicaciones
-    }catch(e){
-      errorInvalidInput.innerText = e.message;  // si las entradas son malas, muestra el msj de error en pantalla
+    } catch (e) {
+      errorInvalidPassword.innerText = e.message;
+      // si las entradas son malas, muestra el msj de error en pantalla
     }
   });
 
-  containerAll.appendChild(formInputCreateAccount); //Se guarda todo el form dentro del container general
+  containerAll.appendChild(formInputCreateAccount); // Se guarda todo el form dentro del container general
 
   return containerAll;
 };
