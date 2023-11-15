4cab026d9bc7f38fe242672eecc5a3140419e7b1
“DanielaP” <padronjodaniela@gmail.com>
2023-11-15 18:29:19 -0500

    se inicia testeo con funciones de boton login y delete de pruebas

--- test/auth.spec.js
+++ test/auth.spec.js
@@ -1,21 +1,37 @@
-/* // importamos la funcion que vamos a testear
-import { loginGoogle, logout, loginEmail } from '..src/firebase/auth.js';
-import { NewAccount } from '..src/componentes/newAccount.js';
-
-describe('myFunction', () => {
-  it('debería ser una función', () => {
-    expect(typeof myFunction).toBe('function');
-  });
-}); */
+/**
+ * @jest-environment jsdom
+ */
 
 // importamos la funcion que vamos a testear
-import { loginEmail } from '../src/firebase/auth.js';
-// import { NewAccount } from '..src/componentes/newAccount.js';
+// import { loginEmail } from '../src/firebase/auth.js';
+import { loginView } from '../src/componentes/login.js';
+import { pupUpDelete } from '../src/componentes/deletePost.js';
 
-jest.mock('..src/firebase/auth.js');
+jest.mock('../src/firebase/auth.js', () => {
 
-describe('Test de los componente loginEmail', () => {
-  it('deberia crear sesion con email correctamente', () => {
-    expect(typeof loginEmail).toBe('function');
+});
+// test de loginView
+describe('Test for loginView', () => {
+  it('it is a function', () => {
+    expect(typeof loginView).toBe('function');
   });
+});
+test('have a button', () => {
+  const DOM = document.createElement('div');
+  DOM.append(loginView());
+  const haveAButton = DOM.querySelector('#loginButton');
+  expect(haveAButton).not.toBe(1);
+});
+
+describe('Test for pupUpDelete', () => {
+  it('it is a delete', () => {
+    console.log(typeof popUpDelete);
+    expect(typeof pupUpDelete).toBe('function');
+  });
+});
+test('have a delete button', () => {
+  const DOM = document.createElement('div');
+  DOM.append(pupUpDelete());
+  const haveAButton = DOM.querySelector('#deleteButton');
+  expect(typeof haveAButton).toBe('object');
 });
